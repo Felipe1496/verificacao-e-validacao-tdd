6ae497247e01645dd4f18187b39daf23acb0ef0d
felipe1496 <felipe.oliveira1496@gmail.com>
2023-04-12 10:31:44 -0300

    criando testes

---
+++ src/test/java/calculadora_salario_test/CalculadoraSalarioTest.java
@@ -0,0 +1,72 @@
+package calculadora_salario_test;
+
+import calculadora_salario.CalculadoraDeSalario;
+import calculadora_salario.Funcionario;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraSalarioTest {
+    @Test
+    public void deveCalcularSalarioLiquidoDesenvolvedorComSalarioMaiorOuIgual3000() {
+        Funcionario funcionario = new Funcionario("João", "joao@teste.com", 4000.0, "DESENVOLVEDOR");
+        double salarioLiquidoEsperado = 3200.0;
+        double salarioLiquidoCalculado = CalculadoraDeSalario.calcularSalarioLiquido(funcionario);
+        Assertions.assertEquals(salarioLiquidoEsperado, salarioLiquidoCalculado, 0.0);
+    }
+
+    @Test
+    public void deveCalcularSalarioLiquidoDesenvolvedorComSalarioMenorQue3000() {
+        Funcionario funcionario = new Funcionario("Maria", "maria@teste.com", 2000.0, "DESENVOLVEDOR");
+        double salarioLiquidoEsperado = 1800.0;
+        double salarioLiquidoCalculado = CalculadoraDeSalario.calcularSalarioLiquido(funcionario);
+        Assertions.assertEquals(salarioLiquidoEsperado, salarioLiquidoCalculado, 0.0);
+    }
+
+    @Test
+    public void deveCalcularSalarioLiquidoDbaComSalarioMaiorOuIgual2000() {
+        Funcionario funcionario = new Funcionario("Pedro", "pedro@teste.com", 3000.0, "DBA");
+        double salarioLiquidoEsperado = 2250.0;
+        double salarioLiquidoCalculado = CalculadoraDeSalario.calcularSalarioLiquido(funcionario);
+        Assertions.assertEquals(salarioLiquidoEsperado, salarioLiquidoCalculado, 0.0);
+    }
+
+    @Test
+    public void deveCalcularSalarioLiquidoDbaComSalarioMenorQue2000() {
+        Funcionario funcionario = new Funcionario("Ana", "ana@teste.com", 1500.0, "DBA");
+        double salarioLiquidoEsperado = 1275.0;
+        double salarioLiquidoCalculado = CalculadoraDeSalario.calcularSalarioLiquido(funcionario);
+        Assertions.assertEquals(salarioLiquidoEsperado, salarioLiquidoCalculado, 0.0);
+    }
+
+    @Test
+    public void deveCalcularSalarioLiquidoTestadorComSalarioMaiorOuIgual2000() {
+        Funcionario funcionario = new Funcionario("Lucas", "lucas@teste.com", 2500.0, "TESTADOR");
+        double salarioLiquidoEsperado = 1875.0;
+        double salarioLiquidoCalculado = CalculadoraDeSalario.calcularSalarioLiquido(funcionario);
+        Assertions.assertEquals(salarioLiquidoEsperado, salarioLiquidoCalculado, 0.0);
+    }
+
+    @Test
+    public void deveCalcularSalarioLiquidoTestadorComSalarioMenorQue2000() {
+        Funcionario funcionario = new Funcionario("Júlia", "julia@teste.com", 1800.0, "TESTADOR");
+        double salarioLiquidoEsperado = 1530.0;
+        double salarioLiquidoCalculado = CalculadoraDeSalario.calcularSalarioLiquido(funcionario);
+        Assertions.assertEquals(salarioLiquidoEsperado, salarioLiquidoCalculado, 0.0);
+    }
+
+    @Test
+    public void deveCalcularSalarioLiquidoGerenteComSalarioMaiorOuIgual5000() {
+        Funcionario funcionario = new Funcionario("Roberto", "roberto@teste.com", 6000.0, "GERENTE");
+        double salarioLiquidoEsperado = 4200.0;
+        double salarioLiquidoCalculado = CalculadoraDeSalario.calcularSalarioLiquido(funcionario);
+        Assertions.assertEquals(salarioLiquidoEsperado, salarioLiquidoCalculado, 0.0);
+    }
+
+    @Test
+    public void deveCalcularSalarioLiquidoGerenteComSalarioMenorQue5000() {
+        Funcionario funcionario = new Funcionario("Carla", "carla@teste.com", 4000.0, "GERENTE");
+        double salarioLiquidoEsperado = 3200.0;
+        double salarioLiquidoCalculado = CalculadoraDeSalario.calcularSalarioLiquido(funcionario);
+        Assertions.assertEquals(salarioLiquidoEsperado, salarioLiquidoCalculado, 0.0);
+    }
+}
