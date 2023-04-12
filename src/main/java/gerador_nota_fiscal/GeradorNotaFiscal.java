7018c0a7bdcef3f054a39a8c37e11798c00aebeb
Eniedson <eniedson.junior@ccc.ufcg.edu.br>
2023-04-12 10:23:31 -0300

    Aplicando alterações para funcionamento do beforeEach

--- src/main/java/gerador_nota_fiscal/GeradorNotaFiscal.java
+++ src/main/java/gerador_nota_fiscal/GeradorNotaFiscal.java
@@ -2,4 +2,13 @@ package gerador_nota_fiscal;
 
 public class GeradorNotaFiscal {
 
+    private NotaFiscalDao notaFiscalDao;
+    private SAP sap;
+    private Smtp smtp;
+
+    public GeradorNotaFiscal(NotaFiscalDao notaFiscalDao, SAP sap, Smtp smtp) {
+        this.notaFiscalDao = notaFiscalDao;
+        this.sap = sap;
+        this.smtp = smtp;
+    }
 }
